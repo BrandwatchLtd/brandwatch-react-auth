ce065be1a2d0bb142aa349ac588b54290a602dcb
thatguynamedandy <andyp@brandwatch.com>
2018-02-07 11:46:25 +0000

    feat(backupDomain): Adds backup domain functionality
    If a user doesn't have access to the specfied product, the backup
    domain allows them to check for auth on an alternative

---
+++ demo/DemoContent.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export class DemoContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: null,
+      token: null,
+    };
+  }
+
+  componentWillMount() {
+    const { brandwatchAuthGetProfile, brandwatchAuthGetToken } = this.context;
+
+    brandwatchAuthGetProfile().then((profile) => this.setState({ profile }));
+    brandwatchAuthGetToken().then((token) => this.setState({ token }));
+  }
+
+  render() {
+    const { brandwatchAuthLogout } = this.context;
+    const { profile, token } = this.state;
+    return (
+      <div>
+        <p>You are authenticated!</p>
+
+        {
+          profile
+            ? <pre><code>{ JSON.stringify(profile, null, 2) }</code></pre>
+            : <p>Fetching profile</p>
+        }
+
+        {
+          token
+            ? <p>Token: { token }</p>
+            : <p>Fetching token</p>
+        }
+
+        <button id="logout" onClick={ () => brandwatchAuthLogout() }>Logout</button>
+      </div>
+    );
+  }
+}
+
+DemoContent.contextTypes = {
+  brandwatchAuthLogout: PropTypes.func.isRequired,
+  brandwatchAuthGetProfile: PropTypes.func.isRequired,
+  brandwatchAuthGetToken: PropTypes.func.isRequired,
+};
--- demo/index.js
+++ demo/index.js
@@ -1,55 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import DemoContent from './DemoContent';
 import BrandwatchReactAuth from '../';
 
-class DemoContent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: null,
-      token: null,
-    };
-  }
-
-  componentWillMount() {
-    const { brandwatchAuthGetProfile, brandwatchAuthGetToken } = this.context;
-
-    brandwatchAuthGetProfile().then((profile) => this.setState({ profile }));
-    brandwatchAuthGetToken().then((token) => this.setState({ token }));
-  }
-
-  render() {
-    const { brandwatchAuthLogout } = this.context;
-    const { profile, token } = this.state;
-
-    return (
-      <div>
-        <p>You are authenticated!</p>
-
-        {
-          profile
-            ? <pre><code>{ JSON.stringify(profile, null, 2) }</code></pre>
-            : <p>Fetching profile</p>
-        }
-
-        {
-          token
-            ? <p>Token: { token }</p>
-            : <p>Fetching token</p>
-        }
-
-        <button onClick={ () => brandwatchAuthLogout() }>Logout</button>
-      </div>
-    );
-  }
-}
-
-DemoContent.contextTypes = {
-  brandwatchAuthLogout: PropTypes.func.isRequired,
-  brandwatchAuthGetProfile: PropTypes.func.isRequired,
-  brandwatchAuthGetToken: PropTypes.func.isRequired,
-};
 
 render((
   <BrandwatchReactAuth
